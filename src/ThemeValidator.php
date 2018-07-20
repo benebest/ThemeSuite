4d4befd9e00fd5959f1b05b17242aa0be69ab3ea
Peter Maselkowski <office@maslosoft.com>
2018-07-20 11:38:07 +0200

    + Scaffolding

---
+++ src/ThemeValidator.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This software package is licensed under `BSD-3-Clause` license[s].
+ *
+ * @package meno-pro/theme-validator
+ * @license BSD-3-Clause
+ *
+ * @copyright Copyright (c) Peter Maselkowski <office@maslosoft.com>
+ * @link https://meno.pro/
+ */
+
+namespace Meno\Validator;
+
+
+class ThemeValidator
+{
+
+}
